87820c3221972e6a9544f04bbc9d2b31eee4a2ea
Stylianos Nektarios Damianakis <snd14641@gmail.com>
2024-05-04 19:31:32 -0400

    feat: created the basic framwork for the app

--- server.js
+++ server.js
@@ -5,17 +5,28 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const app = express();
 
+const indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+const abilitiesRouter = require('./routes/abilities');
+app.use('/abilities', abilitiesRouter);
+const recipesRouter = require('./routes/recipes');
+app.use('/recipes', recipesRouter);
+const setBonusesRouter = require('./routes/setBonuses');
+app.use('/setBonuses', setBonusesRouter);
+const npcsRouter = require('./routes/npcs');
+app.use('/npcs', npcsRouter);
+const questsRouter = require('./routes/quests');
+app.use('/quests', questsRouter);
+const itemsRouter = require('./routes/items');
+app.use('/items', itemsRouter);
+
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static('public'))
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}!`);
-});
-
-app.get('/', (req, res) => {
-  res.render('index')
 });
